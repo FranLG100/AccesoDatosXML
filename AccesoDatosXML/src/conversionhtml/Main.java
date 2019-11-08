3a19e85cf3bd2b1f31fb333143621b709d052a21
fralg100@gmail.com <fralg100@gmail.com>
2019-11-08 20:16:55 +0100

    Falta agregar informacion xml sax

---
+++ AccesoDatosXML/src/conversionhtml/Main.java
@@ -0,0 +1,47 @@
+package conversionhtml;
+
+import java.io.FileOutputStream;
+
+import java.io.OutputStream;
+
+import javax.xml.transform.Source;
+
+import javax.xml.transform.Transformer;
+
+import javax.xml.transform.TransformerFactory;
+
+import javax.xml.transform.stream.StreamResult;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class Main{
+
+ public static void main(String[] args) throws Exception {
+
+  try {
+
+   TransformerFactory tFactory = TransformerFactory.newInstance();
+
+   Source xslDoc = new StreamSource("alumnosPlantilla.xsl");
+
+   Source xmlDoc = new StreamSource("alumnos.xml");
+
+   String outputFileName = "prueba.html";
+
+   OutputStream htmlFile = new FileOutputStream(outputFileName);
+
+   Transformer trasform = tFactory.newTransformer(xslDoc);
+
+   trasform.transform(xmlDoc, new StreamResult(htmlFile));
+
+  } catch (Exception e)
+
+  {
+
+   e.printStackTrace();
+
+  }
+
+ }
+
+}
