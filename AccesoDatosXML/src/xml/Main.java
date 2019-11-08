6512b6fd3d5e7ec5ecc0a18b8aa6a89474fef6f3
fralg100@gmail.com <fralg100@gmail.com>
2019-11-08 19:40:06 +0100

    Escribir desde DOM

--- AccesoDatosXML/src/xml/Main.java
+++ AccesoDatosXML/src/xml/Main.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -18,9 +24,12 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
-		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	      
+		//LEER
 		try {
 			DocumentBuilder builder=factory.newDocumentBuilder();
+			
 			Document doc=builder.parse("alumnos.xml");
 			NodeList alumnos= doc.getElementsByTagName("alumno");
 			for (int i = 0; i < alumnos.getLength(); i++) {
@@ -33,6 +42,7 @@ public class Main {
 				}
 				
 			}
+			
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,6 +50,39 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//ESCRIBIR
+		try {
+			Transformer transformer = transformerFactory.newTransformer();
+			DocumentBuilder builder=factory.newDocumentBuilder();
+			Document doc=builder.parse("alumnos.xml");
+			doc.getDocumentElement().normalize();
+			
+			Node root=doc.getDocumentElement();
+			Element nuevoAlumno=doc.createElement("alumno");
+			
+			Element nombre=doc.createElement("nombre");
+			nombre.setTextContent("Fue un exito");
+			
+			Element edad=doc.createElement("edad");
+			edad.setTextContent("32");
+			
+			nuevoAlumno.appendChild(nombre);
+			nuevoAlumno.appendChild(edad);
+			root.appendChild(nuevoAlumno);
+			
+			DOMSource source=new DOMSource(doc);
+			StreamResult result=new StreamResult("alumnos.xml");
+			transformer.transform(source, result);
+			
+			
+		} catch (TransformerConfigurationException | SAXException | IOException | ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
